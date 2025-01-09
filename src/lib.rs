541d18ab337a1f0c661de8df68f9191af058dd6c
Anders Musikka <anders@andersmusikka.se>
2025-01-09 23:05:38 +0100

    Update doc

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,8 @@
 /// Creates chrono::DateTime values.
 ///
+/// The syntax is inspired by ISO 8601, but because of how rust macros work it cannot
+/// support the exact same format.
+///
 /// Examples:
 ///
 /// ```rust
@@ -32,15 +35,8 @@
 ///         "2024-01-02T00:00:00Z".parse::<chrono::DateTime<chrono::Utc>>().unwrap()
 ///     );
 ///
-///     assert_eq!(
-///         datetime!(2024-01-02),
-///         "2024-01-02T00:00:00".parse().unwrap()
-///     );
-///     assert_eq!(
-///         datetime!(2024-01-02 Z),
-///         "2024-01-02T00:00:00Z".parse::<chrono::DateTime<chrono::Utc>>().unwrap()
-///     );
 /// ```
+/// Note! Because of how rust macros work, you must leave space before/after 'T', and before 'Z'.
 ///
 #[macro_export] macro_rules! datetime {
     ( $year:literal-$month:literal-$day:literal Z)  => {
