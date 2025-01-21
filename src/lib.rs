d6b0fe4c868b7d25e342a4b46449204a28776058
Anders Musikka <anders@andersmusikka.se>
2025-01-21 22:07:17 +0100

    Format

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+pub use chrono;
+
 /// Creates chrono::DateTime values.
 ///
 /// The syntax is inspired by ISO 8601, but because of how rust macros work it cannot
@@ -38,43 +40,44 @@
 /// ```
 /// Note! Because of how rust macros work, you must leave space before/after 'T', and before 'Z'.
 ///
-#[macro_export] macro_rules! datetime {
+#[macro_export]
+macro_rules! datetime {
     ( $year:literal-$month:literal-$day:literal Z)  => {
 
         const {
             #[allow(clippy::zero_prefixed_literal)]
-            chrono::DateTime::<chrono::Utc>::from_naive_utc_and_offset(
+            $crate::chrono::DateTime::<$crate::chrono::Utc>::from_naive_utc_and_offset(
             datetime!($year - $month - $day),
-            chrono::Utc
+            $crate::chrono::Utc
         ) }
     };
     ( $year:literal-$month:literal-$day:literal)  => {
 
         const {
             #[allow(clippy::zero_prefixed_literal)]
-            chrono::NaiveDateTime::new(
+            $crate::chrono::NaiveDateTime::new(
 
-            chrono::NaiveDate::from_ymd_opt($year, $month, $day).expect("date values must be in valid range"),
-            chrono::NaiveTime::from_hms_opt(0,0,0).unwrap()
+            $crate::chrono::NaiveDate::from_ymd_opt($year, $month, $day).expect("date values must be in valid range"),
+            $crate::chrono::NaiveTime::from_hms_opt(0,0,0).unwrap()
         ) }
     };
     ( $year:literal-$month:literal-$day:literal $(T)? $hour:literal:$min:literal:$second:literal Z)  => {
 
         const {
             #[allow(clippy::zero_prefixed_literal)]
-            chrono::DateTime::<chrono::Utc>::from_naive_utc_and_offset(
+            $crate::chrono::DateTime::<$crate::chrono::Utc>::from_naive_utc_and_offset(
 
             datetime!($year - $month - $day $hour:$min:$second),
-            chrono::Utc
+            $crate::chrono::Utc
         ) }
     };
     ( $year:literal-$month:literal-$day:literal $(T)? $hour:literal:$min:literal:$second:literal)  => {
 
         const {
             #[allow(clippy::zero_prefixed_literal)]
-            chrono::NaiveDateTime::new(
-            chrono::NaiveDate::from_ymd_opt($year, $month, $day).expect("date values must be in valid range"),
-            chrono::NaiveTime::from_hms_opt($hour, $min, $second).expect("time values must be in valid range")
+            $crate::chrono::NaiveDateTime::new(
+            $crate::chrono::NaiveDate::from_ymd_opt($year, $month, $day).expect("date values must be in valid range"),
+            $crate::chrono::NaiveTime::from_hms_opt($hour, $min, $second).expect("time values must be in valid range")
         ) }
     };
 }
@@ -83,9 +86,6 @@
 mod tests {
     #[test]
     fn test() {
-        assert_eq!(
-            datetime!(1970-01-01 0:00:00 Z).timestamp(),
-            0
-        );
+        assert_eq!(datetime!(1970-01-01 0:00:00 Z).timestamp(), 0);
     }
 }
